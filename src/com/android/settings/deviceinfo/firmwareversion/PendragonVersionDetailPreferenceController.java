098d7950adf27e2e1bfbd3a02503d192ac14663e
jayb71 <jaybharambe42@gmail.com>
2023-08-28 20:32:25 +0530

    settings: Added Pendragon OS version details in about phone page

---
+++ src/com/android/settings/deviceinfo/firmwareversion/PendragonVersionDetailPreferenceController.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021 PixelPlusUI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ package com.android.settings.deviceinfo.firmwareversion;
+
+ import android.content.Context;
+ import android.os.SystemProperties;
+ import android.text.TextUtils;
+ 
+ import androidx.preference.Preference;
+ 
+ import com.android.settings.R;
+ import com.android.settings.core.BasePreferenceController;
+ 
+ public class PendragonVersionDetailPreferenceController extends BasePreferenceController {
+ 
+     private static final String TAG = "PendragonVersionDetailPreferenceController";
+     //private static final String ROM_PROPERTY = "ro.pendragon.maintainer";
+ 
+     public PendragonVersionDetailPreferenceController(Context context, String key) {
+         super(context, key);
+     }
+ 
+     public int getAvailabilityStatus() {
+         return AVAILABLE;
+     }
+ 
+     public CharSequence getSummary() {
+         String pendragonversion = "13"
+         return pendragonversion;
+     }
+ }
